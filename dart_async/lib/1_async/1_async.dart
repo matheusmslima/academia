867d84a6029c1aa75859f3ed620d820c571e96f0
Matheus Lima <matheusmslima@gmail.com>
2022-03-07 15:28:24 -0300

    Iniciando módulo 6

---
+++ dart_async/lib/1_async/1_async.dart
@@ -0,0 +1,14 @@
+void main() {
+  print('Iniciou o processo.');
+  var i = 0;
+  while(i < 5){
+    print(i);
+    i++;
+  }
+  m2();
+  print('Finalizou');
+}
+
+void m2(){
+  print('funcao 2');
+}
