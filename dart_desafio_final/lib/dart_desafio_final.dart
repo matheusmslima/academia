e269f127f650ffae622186ea5debeec9968e5390
Matheus Lima <matheusmslima@gmail.com>
2022-03-11 16:04:29 -0300

    added mentoria_json inicio

--- dart_desafio_final/lib/dart_desafio_final.dart
+++ dart_desafio_final/lib/dart_desafio_final.dart
@@ -1,5 +1,16 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-void main(){
+Future<void> main() async {
+  buscarEstado();
+}
 
+Future<void> buscarEstado() async {
+  var url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/';
+  var response = await http.get(Uri.parse(url));
+
+  var responseData = convert.jsonDecode(response.body);
+
+  print(responseData['sigla']);
+  // print(responseData.runtimeType);
 }
